b5d0bd858c7df1d05fffc4dc8a8f962de8243f3a
KDr2 <kdr2@x-macro.com>
2009-04-28 20:25:33 +0800

    init

---
+++ configure.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+#
+# project : erlix
+# author : kdr2
+#
+
+findei=File.join(File.dirname(__FILE__),"findei.erl")
+File.chmod(0755,findei)
+ei_dir=`#{findei}`.chomp
+src_dir=File.join(File.dirname(__FILE__),"src")
+mkmf=File.join(File.dirname(__FILE__),"extconf.rb")
+puts `ruby #{mkmf} --with-ei-dir=#{ei_dir} --with-ldflags=\"-lei -lerl_interface\" --srcdir=#{src_dir}`
